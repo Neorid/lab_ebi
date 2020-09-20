c17a7afa75058e472345feacfd97c72a974254df
Neorid <allneorid@yandex.ru>
2020-09-20 19:27:14 +0500

    add files

--- api/main.py
+++ api/main.py
@@ -1,16 +1,29 @@
 # This is a sample Python script.
-
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import jsonify as jsonify
+
+from core.iteractors.board_iter import BoardIter
+from services.request.board_find import BoardFind
+
+boundary = BoardIter()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def print_hi(phone_id):
+    try:
+        response = boundary.find(BoardFind(phone_id))
+        res = {'id': response.id,
+               'name': response.name,
+               'price': response.price,
+               'quantity': response.quantity,
+               'years': response.years}
+        return jsonify({'phone': res})
+    except Exception as e:
+        return str(e)
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
--- core/iteractors/board_iter.py
+++ core/iteractors/board_iter.py
@@ -1,3 +1,4 @@
+from core.entities.boards import Phone
 from services.boundaries.board_bound import BoardBound
 from services.request.board_find import BoardFind
 from services.request.boards_create import BoardCreate
@@ -6,11 +7,26 @@ from services.responses.board_res import BoardRes
 
 
 class BoardIter(BoardBound):
-
+    base = {}
+    count = 0
 
     def create(self, request: BoardCreate) -> BoardRes:
+        phone = Phone(name=request.name,
+                      price=request.price,
+                      quantity=request.quantity,
+                      years=request.years,
+                      id=self.count)
+        self.base[self.count] = phone
+        self.count += 1
+        return BoardCreate(phone.id)
 
-        pass
-
-    def find(self, request: BoardFind) -> BoardFindRes:
-        pass
+    def find(self, request: BoardFind) -> BoardFind:
+        id = request.id
+        phone = self.base.get(id, None)
+        if phone:
+            return BoardFind(id=phone.id,
+                             name=phone.name,
+                             price=phone.price,
+                             quantity=phone.quantity,
+                             years=phone.years)
+        raise Exception('Phone error')
